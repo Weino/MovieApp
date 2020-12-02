20251ee65775f09b6152dc9e6024feab1c051493
William Weinö <William.Weino@hotmail.com>
2020-12-02 16:41:04 +0100

    The start of the database and my first commit

---
+++ MovieRentalAppProject/Context.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using static MovieRentalAppProject.Customer;
+
+namespace MovieRentalAppProject
+{
+    public class Context : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Rental> Sales { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(
+                @"server=.\SQLEXPRESS;" +
+                @"database=MySaleDatabase;" +
+                @"trusted_connection=true;" +
+                @"MultipleActiveResultSets=True"
+                );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(new Customer
+            {
+                Id = 1,
+                FirstName = "William",
+                LastName = "Weinö",
+                Gender = Gender.Male
+            });    
+                
+                
+                
+
+        }
+    }
+}
---
+++ MovieRentalAppProject/Model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRentalAppProject
+
+    {
+        public enum Gender
+        {
+            Male,
+            Female,
+        }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual List<Rental> Sales { get; set; }
+        public Gender Gender { get; set; }
+
+    }
+
+    public class Rental
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual Movie Movie { get; set;}
+
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        public virtual List<Rental> Sales { get; set; }
+    }
+}
