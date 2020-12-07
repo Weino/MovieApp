b7ea7081f1c9b5e77f48f54e546740fdca136ae8
William Weinö <William.Weino@hotmail.com>
2020-12-07 17:42:33 +0100

    added Mouse enter function on the menu

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace Store
         {
             InitializeComponent();
         }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (tg_btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_news.Visibility = Visibility.Collapsed;
+                tt_movies.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+                tt_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_news.Visibility = Visibility.Visible;
+                tt_movies.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+                tt_signout.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
